6712eca575c753e936940fa3d8ba86688b5402b2
Martin D <martin.dubosq.lgm@gmail.com>
2025-06-29 19:51:04 +0200

    aller ogm si c sa3

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-// const { getDefaultConfig } = require("expo/metro-config");
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require("expo/metro-config");
 
-// const config = getDefaultConfig(__dirname);
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
